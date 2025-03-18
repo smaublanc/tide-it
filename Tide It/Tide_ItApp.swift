c29eeb15a1e6aa53a01519c7dc45b84f006acb1f
Maublanc <leo3d@me.com>
2025-03-18 16:41:49 +0100

    Initial Commit

---
+++ Tide It/Tide_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tide_ItApp.swift
+//  Tide It
+//
+//  Created by Sébastien Maublanc on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tide_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
